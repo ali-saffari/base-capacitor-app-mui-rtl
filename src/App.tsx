5d6c48e55bbc034096a8c3a1f29cde5d937317a5
ali saffari <alisaffari1377@gmail.com>
2022-02-12 19:21:39 +0330

    mui ltr to rtl

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
 
 function App() {
   return (
@@ -15,6 +19,9 @@ function App() {
         <Button variant="text">Text</Button>
         <Button variant="contained">Contained</Button>
         <Button variant="outlined">Outlined</Button>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
+import rtlPlugin from 'stylis-plugin-rtl';
+import {CacheProvider} from '@emotion/react';
+import createCache from '@emotion/cache';
+import {prefixer} from 'stylis';
+
+
+var rtl = true
+
+
+let optCache = {
+    key: 'muiltr',
+    stylisPlugins: []
+}
+if (rtl) {
+    // @ts-ignore
+    optCache.stylisPlugins = [prefixer, rtlPlugin]
+}
+
+
+const cacheRtl = createCache(optCache);
+
+
+const theme = createTheme({
+    direction: rtl ? 'rtl' : 'ltr',
+    palette: {
+        mode: 'dark',
+    },
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <div dir={rtl ? 'rtl' : 'ltr'}>
+            <CacheProvider value={cacheRtl}>
+                <ThemeProvider theme={theme}>
+                    <App/>
+                </ThemeProvider>
+            </CacheProvider>
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
