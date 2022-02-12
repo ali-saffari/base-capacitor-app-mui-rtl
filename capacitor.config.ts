0146dfa024afcdd25fae20cadbb1449307742237
ali saffari <alisaffari1377@gmail.com>
2022-02-12 18:56:37 +0330

    init capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'my-typescript-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
