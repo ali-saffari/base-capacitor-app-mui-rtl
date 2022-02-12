4851870b4241985bc71f4471264f9cc44f5298d4
ali saffari <alisaffari1377@gmail.com>
2022-02-12 21:23:25 +0330

    Arrange and clear the base

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'my-typescript-app',
-  webDir: 'build',
+  webDir: "build",
+  plugins: {
+    "SplashScreen": {
+      "launchShowDuration": 0
+    }
+  },
   bundledWebRuntime: false
 };
 
--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,12 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import LocalizedDatePicker from "./datePicker";
-
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ویرایش <code>src/App.tsx</code> and save to reload.
-        </p>
-        <LocalizedDatePicker/>
-
-
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      app
     </div>
   );
 }
